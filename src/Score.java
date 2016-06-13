9e0b95a06f1646a31cb6b433c55120998ff9824d
lovejyu <f74031035@mail.ncku.edu.tw>
2016-06-14 01:01:25 +0800

    g first

---
+++ src/Score.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.*;
+public class Score extends JLabel{
+	private int score;
+	public Score(){
+		setLocation(800,0);
+		setSize(150, 150);
+		setText("Score:00");
+		setFont(new Font("Arial Black",Font.PLAIN,30));
+		setForeground(Color.YELLOW);
+	}
+	public void addScore(){
+		score=score+10;
+		
+		this.setText("Score:"+score);
+	}
+}
