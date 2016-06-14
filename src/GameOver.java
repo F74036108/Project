64ed13eeb127649af2224fe172555d9499280366
collin85820 <f74034091@mail.ncku.edu.tw>
2016-06-14 16:40:22 +0800

    add comment

--- src/GameOver.java
+++ src/GameOver.java
@@ -15,34 +15,39 @@ public class GameOver extends JFrame{
 			this.game = game;
 			this.setSize(x, y);
 			this.setContentPane(new JLabel(new ImageIcon(".\\image\\seabg.jpg")));
+			//game over 圖案
 			JLabel gameover = new JLabel();
-			JLabel health = new JLabel();
-			JLabel word = new JLabel();
 			ImageIcon icon = new ImageIcon(".\\image\\game over.png");
-			ImageIcon icon2 = new ImageIcon(".\\image\\YES.png");
-			ImageIcon icon3 = new ImageIcon(".\\image\\NO.png");
-			ImageIcon icon4 = new ImageIcon(".\\image\\0.png");
-			ImageIcon icon5 = new ImageIcon(".\\image\\again.png");
-
 			gameover.setIcon(icon);
 			gameover.setSize(547,245);
 			gameover.setLocation(x/2-250,y/2-300);
+			//歸零血條設置
+			ImageIcon icon4 = new ImageIcon(".\\image\\0.png");
+			JLabel health = new JLabel();
 			health.setIcon(icon4);
 			health.setLocation(0,0);
 			health.setSize(400, 22);
+			//try again文字
+			JLabel word = new JLabel();
+			ImageIcon icon5 = new ImageIcon(".\\image\\again.png");
 			word.setIcon(icon5);
 			word.setLocation(x/2-270,y/2+20);
 			word.setSize(547, 80);
+			//重開始按鈕
+			ImageIcon icon2 = new ImageIcon(".\\image\\YES.png");
 			JButton restart = new JButton(icon2);
-			JButton exitbtn = new JButton(icon3);
 			Restart_listener mblistener = new Restart_listener(this);
 			restart.addActionListener(mblistener);
 			restart.setLocation(x/2-300,y/2+150);
 			restart.setSize(234,58);
+			//離開按鈕
+			ImageIcon icon3 = new ImageIcon(".\\image\\NO.png");
+			JButton exitbtn = new JButton(icon3);
 			ExitListener exitlistener = new ExitListener(this);
 			exitbtn.addActionListener(exitlistener);
 			exitbtn.setLocation(x/2+54,y/2+150);
 			exitbtn.setSize(234,58);
+			//加入所有按鈕及畫面
 			this.add(exitbtn);
 			this.add(restart);
 			this.add(gameover);
