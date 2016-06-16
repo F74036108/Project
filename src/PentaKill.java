7b7102e452ae2c39a0b38c4f137ae1630af4b8f3
F74036108 <F74036108@mail.ncku.edu.tw>
2016-06-17 06:08:23 +0800

    add Penta image

--- src/PentaKill.java
+++ src/PentaKill.java
@@ -7,26 +7,32 @@ public class PentaKill {
 	boolean E;
 	private final int NUM_OF_PENTAKILL=3;
 	private int killCount;
-
-	ImageIcon icon2 = new ImageIcon(".\\image\\big-kill2.gif");
+	JLabel pentaKill = new JLabel();
+	ImageIcon icon1 = new ImageIcon(".\\image\\many bombs1.png");
+	ImageIcon icon2 = new ImageIcon(".\\image\\many bombs2.png");
+	ImageIcon icon3 = new ImageIcon(".\\image\\many bombs3.png");
+	ImageIcon iconShipPenta = new ImageIcon(".\\image\\big-kill2.gif");
 			
 	public PentaKill(SubmarineMain game) {
 		this.game = game;
-		JLabel pentaKill = new JLabel();
+		
 		ImageIcon icon = new ImageIcon(".\\image\\submarine2.png");// SET image;
-		pentaKill.setIcon(icon);
-		pentaKill.setLocation(0, 600);
-		pentaKill.setSize(142, 49);
+		pentaKill.setIcon(icon3);
+		pentaKill.setLocation(850, 100);
+		pentaKill.setSize(80, 110);
 		this.game.add(pentaKill);
 	}
 	public void bombFromSky(){
 		if(killCount<NUM_OF_PENTAKILL){
-			game.ship.setIcon(icon2);
+			game.ship.setIcon(iconShipPenta);
 			
 			for(int i=0;i<=8;i++){
 				game.bomb.addBomb(110*i+10, (int)(Math.random()*150));
 			}
 			killCount++;
+			if(killCount==1)pentaKill.setIcon(icon2);
+			if(killCount==2)pentaKill.setIcon(icon1);
+			if(killCount==3)pentaKill.setVisible(false);
 			
 			//game.ship.resetIcon();
 			reset();
